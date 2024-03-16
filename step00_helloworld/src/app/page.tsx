77da1e809aa3830dde11ccf6b0f36644da3ff73a
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-03-16 18:06:55 +0500

    add test page

--- step00_helloworld/src/app/page.tsx
+++ step00_helloworld/src/app/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Hello from Zia Khan</div>;
+  return (
+    <div className="flex flex-col gap-3 text-sm p-10">
+      <div>Hello from Zia Khan</div>
+      <div>
+        <Link href={"test_page"}>Go to Test Page</Link>
+      </div>
+    </div>
+  );
 }
---
+++ step00_helloworld/src/app/test_page/page.tsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+  return (
+    <div className="flex justify-center items-center h-screen text-xl">
+      Welcome to Test Page
+    </div>
+  );
+}
