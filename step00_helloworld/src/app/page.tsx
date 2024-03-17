b5d4da173090f55176c766e764769a29585eea83
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-03-17 22:44:22 +0500

    deploy on vercel by importing git repo

--- step00_helloworld/src/app/page.tsx
+++ step00_helloworld/src/app/page.tsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-3 text-xl p-10">
-      <div>Hello from Zia Khan</div>
+    <div className="flex flex-col h-screen justify-center items-center text-xl">
+      <div className="p-10">
+        My name is Sarfaraz ul Haq and this is my first Next.js project.
+      </div>
       <div>
         <Link
           className="border border-gray-300 px-2 py-1 rounded-md hover:bg-gray-300 hover:border-gray-300"
