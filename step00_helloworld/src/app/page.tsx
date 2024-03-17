a20e2700eb8bd22d9839fe226b19c919e686712e
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-03-17 10:19:49 +0500

    change style

--- step00_helloworld/src/app/page.tsx
+++ step00_helloworld/src/app/page.tsx
@@ -2,10 +2,15 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-3 text-sm p-10">
+    <div className="flex flex-col gap-3 text-xl p-10">
       <div>Hello from Zia Khan</div>
       <div>
-        <Link href={"test_page"}>Go to Test Page</Link>
+        <Link
+          className="border border-gray-300 px-2 py-1 rounded-md hover:bg-gray-300 hover:border-gray-300"
+          href={"test_page"}
+        >
+          Go to Test Page
+        </Link>
       </div>
     </div>
   );
--- step00_helloworld/src/app/test_page/page.tsx
+++ step00_helloworld/src/app/test_page/page.tsx
@@ -1,6 +1,6 @@
 export default function TestPage() {
   return (
-    <div className="flex justify-center items-center h-screen text-xl">
+    <div className="flex justify-center items-center h-screen text-5xl">
       Welcome to Test Page
     </div>
   );
