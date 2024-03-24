2b4c9d091a7e7836b63a6a9877bfa8b7563f76af
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-03-24 17:48:28 +0500

    add dashboard and settings

---
+++ step01_routes/src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function DashboardPage() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen text-bold text-5xl">
+      Hello, Dashboard Page!
+    </div>
+  );
+}
---
+++ step01_routes/src/app/settings/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex flex-col justify-center items-center h-screen text-5xl">
+      Hello, Settings Page!
+    </div>
+  );
+}
