860da94d0a63d83220d4574a481de931fb383a03
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-03-31 16:40:39 +0500

    remove curly braces href

--- step02_linking/src/app/page.tsx
+++ step02_linking/src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <div>Hello World from Panaverse DAO!</div>
       <div>
         <button className="inline-block px-4 py-2 bg-gray-300 rounded-md hover:bg-gray-800 hover:text-white">
-          <Link href={"/name"}>Go to name page</Link>
+          <Link href="/name">Go to name page</Link>
         </button>
       </div>
     </div>
