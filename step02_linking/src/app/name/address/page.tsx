f0ee010f3778b3de649532a087c7fecec9cdaf2f
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-03-31 16:44:11 +0500

    change name

--- step02_linking/src/app/name/address/page.tsx
+++ step02_linking/src/app/name/address/page.tsx
@@ -1,7 +1,7 @@
 export default function GiveAddress() {
   return (
     <div className="flex flex-col justify-center items-center h-screen space-y-3 text-center">
-      I live in Karachi, Pakistan
+      I live in Lahore, Pakistan
     </div>
   );
 }
--- step02_linking/src/app/name/page.tsx
+++ step02_linking/src/app/name/page.tsx
@@ -6,7 +6,7 @@ export default function GiveName() {
   const router = useRouter();
   return (
     <div className="flex flex-col justify-center items-center h-screen space-y-3 text-center">
-      My name is Zia.
+      My name is Sarfaraz.
       <br />
       <button
         className="inline-block px-4 py-2 bg-gray-300 rounded-md hover:bg-gray-800 hover:text-white"
