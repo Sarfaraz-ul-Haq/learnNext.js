c049c82f11d708e3fd84ae4530b6d9d47c4f9fec
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-04-01 05:39:43 +0500

    add text

--- step02_linking/src/app/page.tsx
+++ step02_linking/src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen space-y-3 text-center">
       <div>Hello World from Panaverse DAO!</div>
+      <div>Learning to link routes</div>
       <div>
         <button className="inline-block px-4 py-2 bg-gray-300 rounded-md hover:bg-gray-800 hover:text-white">
           <Link href="/name">Go to name page</Link>
