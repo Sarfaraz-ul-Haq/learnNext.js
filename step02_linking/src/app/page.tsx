972c77015ae76205b80ae8bddf516722d220ff97
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-04-01 05:43:26 +0500

    add new folders

--- step02_linking/src/app/page.tsx
+++ step02_linking/src/app/page.tsx
@@ -5,11 +5,6 @@ export default function Home() {
     <div className="flex flex-col justify-center items-center h-screen space-y-3 text-center">
       <div>Hello World from Panaverse DAO!</div>
       <div>Learning to link routes</div>
-      <div>
-        <button className="inline-block px-4 py-2 bg-gray-300 rounded-md hover:bg-gray-800 hover:text-white">
-          <Link href="/name">Go to name page</Link>
-        </button>
-      </div>
     </div>
   );
 }
