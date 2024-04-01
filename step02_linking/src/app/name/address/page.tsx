489c5e3eff7312dbf55895e1413303d5535e6741
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-04-01 05:39:12 +0500

    remove name folder

--- step02_linking/src/app/name/address/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function GiveAddress() {
-  return (
-    <div className="flex flex-col justify-center items-center h-screen space-y-3 text-center">
-      I live in Lahore, Pakistan
-    </div>
-  );
-}
