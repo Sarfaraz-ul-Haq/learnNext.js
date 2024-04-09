ed1c121903e86d76db7c3816faf28ae895959fb2
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-04-09 06:14:33 +0500

    change button color

--- step01_routes/src/app/page.tsx
+++ step01_routes/src/app/page.tsx
@@ -5,17 +5,17 @@ export default function HomePage() {
     <div className="p-10 space-y-5 flex flex-col justify-center items-center h-screen">
       <h1 className="font-bold text-4xl">Hello, Home Page!</h1>
       <div>
-        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
+        <button className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
           <Link href={"/dashboard"}>Go to Dashboard Page</Link>
         </button>
       </div>
       <div>
-        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
+        <button className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
           <Link href={"/blog"}>Go to Blog Page</Link>
         </button>
       </div>
       <div>
-        <button className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
+        <button className="bg-gray-200 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded">
           <Link href={"/contact_us"}>Go to Contact Us Page</Link>
         </button>
       </div>
