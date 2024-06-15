909180317f19bd01e7004b43c49c3b602b23f79d
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-06-15 12:28:17 +0500

    practice

--- step08_invoking_api/step03_fetching_data_server_dynamic/src/app/page.tsx
+++ step08_invoking_api/step03_fetching_data_server_dynamic/src/app/page.tsx
@@ -1,5 +1,23 @@
-function Home() {
-  return <div>Home</div>;
+import { useState } from "react";
+
+const getData = async () => {
+  const res = await fetch("https://api.quotable.io/random?tags=technologyy", {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+};
+
+async function Home() {
+  const data = await getData();
+
+  console.log(data.content);
+
+  return <div className="p-24">{data.content}</div>;
 }
 
 export default Home;
